1ed7728d42e1f77cc90807c796a50cebee1000d0
AsueDerick <asuederipotas7@gmail.com>
2025-08-15 13:54:32 +0400

    update

--- rds.tf
+++ rds.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "postgres" {
   username                = "admin"
   password                = "ChangeMe123!"  # ideally, use secrets manager or Terraform variable
   db_subnet_group_name    = aws_db_subnet_group.rds_subnets.name
-  vpc_security_group_ids  = [module.myapp-vpc.vpc_default_security_group_id]
+  vpc_security_group_ids  = [module.myapp-vpc.default_security_group_id]
   multi_az                = false
   skip_final_snapshot     = true
   publicly_accessible     = false
