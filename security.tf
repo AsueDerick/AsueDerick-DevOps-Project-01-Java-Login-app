14ff1b13a19a238f1ec1ac2112b6e7145fca5850
AsueDerick <asuederipotas7@gmail.com>
2025-08-15 11:50:44 +0400

    updated

--- security.tf
+++ security.tf
@@ -1,13 +1,12 @@
 resource "aws_security_group" "rds_postgres_sg" {
   name        = "rds-postgres-sg"
   description = "Security group for RDS PostgreSQL"
-  vpc_id      = module.my_vpc.vpc_id 
+  vpc_id      = module.myapp-vpc.vpc_id
   ingress {
     description      = "PostgreSQL from EKS nodes"
     from_port        = 5432
     to_port          = 5432
     protocol         = "tcp"
-    security_groups  = [aws_security_group.eks_nodes_sg.id] # SG of your EKS worker nodes
   }
     egress {
         from_port   = 0
