5dc8ecae8ccec333340401bc5fe1325220e429c5
AsueDerick <asuederipotas7@gmail.com>
2025-08-15 15:07:44 +0400

    update

--- rds.tf
+++ rds.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "postgres" {
   instance_class          = "db.t3.medium"
   allocated_storage       = 20
   storage_type            = "gp2"
-  username                = "admin"
+  username                = "mydbuser"  # ideally, use secrets manager or Terraform variable
   password                = "ChangeMe123!"  # ideally, use secrets manager or Terraform variable
   db_subnet_group_name    = aws_db_subnet_group.rds_subnets.name
   vpc_security_group_ids  = [module.myapp-vpc.default_security_group_id]
